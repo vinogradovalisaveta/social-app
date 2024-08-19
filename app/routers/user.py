c051e1f6aec1b29a99478d6ded87066df85dae25
Lisaveta Yurashevich <vinogradova.lisaveta@gmail.com>
2024-08-19 17:55:21 +0300

    проверка на уникальность юзернейма и имейла

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.exceptions import HTTPException
 
 from app.database import get_session
 from app.schemas.user import UserCreateSchema
-from app.services.user import create_user
+from app.services.user import create_user, get_user_by_username, get_user_by_email
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -12,4 +13,10 @@ router = APIRouter(prefix="/users", tags=["users"])
 async def register(
     user: UserCreateSchema, session: AsyncSession = Depends(get_session)
 ):
-    return await create_user(session, user)
+    new_username = await get_user_by_username(session, username=user.username)
+    new_email = await get_user_by_email(session, email=user.email)
+    if new_username:
+        raise HTTPException(status_code=400, detail="this username is already taken")
+    elif new_email:
+        raise HTTPException(status_code=400, detail="this email is already taken")
+    return await create_user(session=session, user=user)
--- app/services/user.py
+++ app/services/user.py
@@ -1,8 +1,27 @@
+from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.models.models import User
 from app.schemas.user import UserCreateSchema
 from app.services.auth import encrypt_password
+
+
+async def get_user_by_username(session: AsyncSession, username: str):
+    user = await session.execute(select(User).where(User.username == username))
+    user = user.scalar()
+    return user
+
+
+async def get_user_by_email(session: AsyncSession, email: str):
+    user = await session.execute(select(User).where(User.email == email))
+    user = user.scalar()
+    return user
+
+
+async def get_user_by_id(session: AsyncSession, user_id: int):
+    user = await session.execute(select(User).where(User.id == user_id))
+    user = user.scalar()
+    return user
 
 
 async def create_user(session: AsyncSession, user: UserCreateSchema):
