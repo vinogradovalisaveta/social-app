392176177a3458af08350e6304f5bb141ac5f19d
Lisaveta Yurashevich <vinogradova.lisaveta@gmail.com>
2024-08-23 17:57:21 +0300

    def get_user_profile added

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,10 +1,17 @@
+from typing import Optional
+
 from fastapi import APIRouter, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.exceptions import HTTPException
+from fastapi import HTTPException
 
 from app.database import get_session
-from app.schemas.user import UserCreateSchema
-from app.services.user import create_user, get_user_by_username, get_user_by_email
+from app.schemas.user import UserCreateSchema, UserSchema
+from app.services.user import (
+    create_user,
+    get_user_by_username,
+    get_user_by_email,
+    get_user_by_id,
+)
 
 router = APIRouter(prefix="/users", tags=["users"])
 
@@ -20,3 +27,21 @@ async def register(
     elif new_email:
         raise HTTPException(status_code=400, detail="this email is already taken")
     return await create_user(session=session, user=user)
+
+
+@router.get("/user/user", response_model=UserSchema)
+async def get_user_profile(
+    username: Optional[str] = None,
+    user_id: Optional[int] = None,
+    session: AsyncSession = Depends(get_session),
+):
+    if username:
+        db_user = await get_user_by_username(session=session, username=username)
+    elif user_id:
+        db_user = await get_user_by_id(session=session, user_id=user_id)
+    else:
+        raise HTTPException(status_code=404, detail="user not found")
+
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="usernot found")
+    return db_user
